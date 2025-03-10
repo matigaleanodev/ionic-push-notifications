bcea7735adf18471e7b1eaf80db558b49d88d8dc
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2025-03-10 17:39:00 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ionic Push Notification',
+  webDir: 'www'
+};
+
+export default config;
